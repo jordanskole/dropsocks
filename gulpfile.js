537c10749dd6a59c6ce4acc34853d7e4401b5e70
Jordan Skole <jordan@usebenchmark.com>
2015-09-29 10:33:28 -0400

    gulp connect

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,20 @@
-'use strict';
-
 var gulp = require('gulp'),
-    livereload = require('gulp-livereload'),
-    http = require('http'),
-    st = require('st');
+  connect = require('gulp-connect');
 
-gulp.task('less', function() {
-  gulp.src('app/styles/*.less')
-    .pipe(less())
-    .pipe(gulp.dest('dist/styles'))
-    .pipe(livereload());
+gulp.task('connect', function() {
+  connect.server({
+    root: __dirname,
+    livereload: true
+  });
 });
 
-gulp.task('watch', ['server'], function() {
-  livereload.listen({ basePath: 'dist' });
-  gulp.watch('less/*.less', ['less']);
+gulp.task('html', function () {
+  gulp.src('*.html')
+    .pipe(connect.reload());
 });
 
-gulp.task('serve', function(done) {
-  http.createServer(
-    st({ path: __dirname, index: 'index.html', cache: false })
-  ).listen(8080, done);
+gulp.task('watch', function () {
+  gulp.watch(['*.html'], ['html']);
 });
+
+gulp.task('default', ['connect', 'watch']);
